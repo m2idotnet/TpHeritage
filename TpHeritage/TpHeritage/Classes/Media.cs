b7c3447dc3df0afd9454fde9bc2315258630b771
m2i <m2idotnet@gmail.com>
2019-04-30 09:48:46 +0200

    Correction Ex1

---
+++ TpHeritage/TpHeritage/Classes/Media.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TpHeritage.Classes
+{
+    public class Media
+    {
+        private string titre;
+        private int numero;
+        private static int compteur = 0;
+
+        //Propriété en lecture seul sans set
+        public string Titre { get => titre; }
+        public int Numero { get => numero; }
+
+        public Media(string t)
+        {
+            //attribut car propriété en lecture seul
+            titre = t;
+            numero = compteur;
+            compteur++;
+        }
+
+        //methode utilisée quand un objet de type media est utilisé comme une chaine de caractères
+        public override string ToString()
+        {
+            return "Numéro : "+ Numero + " titre : " + Titre;
+        }
+
+        public bool plusPetit(Media doc)
+        {
+            return (Numero < doc.Numero);
+        }
+    }
+}
