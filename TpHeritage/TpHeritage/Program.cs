2b349eacc54967a70b9808105395fd4ebc635dd5
m2i <m2idotnet@gmail.com>
2019-04-30 09:33:17 +0200

    Version initial

---
+++ TpHeritage/TpHeritage/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TpHeritage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
